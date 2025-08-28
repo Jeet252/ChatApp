dc54cf2c41cdfeeb3f568afca85ae4cfdffd553f
Jeet252 <jeetnakrani2002@gmail.com>
2025-08-28 22:10:45 +0530

    update CORS configuration to allow multiple origins

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://stoa-vrksah.app"],
     credentials: true,
   },
 });
